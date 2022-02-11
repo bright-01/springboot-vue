5ca25e69ffb03ea5a1c524845812717bc1e0e80b
쿠도커뮤니케이션 천명원 <mwcheon@cudo.co.kr>
2022-02-11 14:28:38 +0900

    test commit

---
+++ src/main/java/com/example/sptringbootvue/SptringbootVueApplication.java
@@ -0,0 +1,13 @@
+package com.example.sptringbootvue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SptringbootVueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SptringbootVueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sptringbootvue/SptringbootVueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sptringbootvue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SptringbootVueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
